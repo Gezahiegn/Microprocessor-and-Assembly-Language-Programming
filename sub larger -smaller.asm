e38ebbd802d1c81a3ceaab19c8fbacc5405d4440
Gezahiegn <110999821+Gezahiegn@users.noreply.github.com>
2024-01-10 17:17:58 +0300

    Add files via upload

---
+++ sub larger -smaller.asm
@@ -0,0 +1,8 @@
+org 100
+  ; subtruct from larger
+  mov ax,0b2h
+  mov bx,77h
+  sub ax,bx 
+ret  
+ 
+ 
---
+++ sub smaller-larger.asm
@@ -0,0 +1,7 @@
+org 100h 
+;subtract larger number from smaller number
+ mov ax,77h
+ mov bx,0b2h
+ sub ax,bx 
+ ;dec ax
+ret
